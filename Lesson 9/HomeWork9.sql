17554730a7c422f15c059aa353c4702b0fe77551
kseniya.shulitskaya <shulitskaya@gmail.com>
2024-02-04 13:41:53 +0300

    added HomeWork9

---
+++ Lesson 9/HomeWork9.sql
@@ -0,0 +1,6 @@
+﻿--2.
+SELECT 
+CONCAT(p.FirstName, p.LastName) as ФИО,
+CONCAT(pf.FirstName, pf.LastName) as ФИО_Отца  
+FROM People p LEFT JOIN People pf ON p.ID_Father=pf.ID
+WHERE p.FirstName LIKE 'Дмитрий'
